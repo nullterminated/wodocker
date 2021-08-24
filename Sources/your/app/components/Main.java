e50e56c77c56ad226ad219d6e1511d128e768f98
Ramsey Gurley <ramseygurley@gmail.com>
2021-08-24 16:41:00 +0900

    Add a small test form

--- Sources/your/app/components/Main.java
+++ Sources/your/app/components/Main.java
@@ -3,14 +3,34 @@ package your.app.components;
 import com.webobjects.appserver.WOContext;
 
 import er.extensions.components.ERXComponent;
+import com.webobjects.appserver.WOActionResults;
 
 public class Main extends ERXComponent {
 	/**
 	 * Default serial version id
 	 */
 	private static final long serialVersionUID = 1L;
+	private String formText;
 
 	public Main(WOContext context) {
 		super(context);
+	}
+
+	/**
+	 * @return the formText
+	 */
+	public String formText() {
+		return formText;
+	}
+
+	/**
+	 * @param formText the formText to set
+	 */
+	public void setFormText(String formText) {
+		this.formText = formText;
+	}
+
+	public WOActionResults echo() {
+		return null;
 	}
 }
